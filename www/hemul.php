6959f1746d96b91f20bf7f6ae7ae57cbf52e8f6a
Rémi Puig <remi@fria.nu>
2015-03-06 02:26:20 +0100

    börjat jobba med smhi

---
+++ www/hemul.php
@@ -0,0 +1,24 @@
+<?php
+class hemul {
+	public function __construct($lat, $lon, $adr) {
+		$this->lat = $lat;
+		$this->lon = $lon;
+		$this->adr = $adr;
+	}	
+
+	public function getRain() {
+		$key = $this::smhiGetKey();
+		$url = "http://opendata-download-metobs.smhi.se/api/version/latest/parameter/5/station/$key.json";
+		return "it's raining";		
+	}
+
+	public function getSnow() {
+		$url = "http://opendata-download-metobs.smhi.se/api/version/latest/parameter/8/station/$key.json";
+	}
+
+	private function smhiGetKey() {
+		/* http://opendata.smhi.se/apidocs/metobs/version.html */
+		/* todo cacha all the tations from http://opendata-download-metobs.smhi.se/api/version/latest/parameter/5 mysql? */
+	}
+
+}
