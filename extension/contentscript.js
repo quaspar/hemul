483d667d92c9399146e38f665ae8d01d1d7bbba9
quaspar <caspar.behrendt@gmail.com>
2015-03-06 10:00:24 +0100

    rättade js-syntaxfel

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,16 +1,17 @@
-function handle_ajax_succes(response = null){
-	alert("success!");
+function handle_ajax_succes(response){
+	alert("success!"); // debug
+	console.log(response); // debug
 	return null;
 };
 
-function send_props_to_server(){
+function send_props_to_server(success_callback){
 	var script_tag = $('script[type="text/javascript"]:contains("window.initMap")');
 	var match=/var properties = (.*)/.exec(script_tag[0].text);
 	var postdata = "properties=" + match[1];
 	console.log(postdata); // debug
 	// Note that any URL fetched here must be matched by a permission in
 	// the manifest.json file!
-	var jqxhr = $.post('http://hemul.fria.nu/ajaxhandler.php', postdata, handle_ajax_success).fail(function() {
+	var jqxhr = $.post('http://hemul.fria.nu/ajaxhandler.php', postdata, success_callback).fail(function() {
     	alert( "error");
 	})
 }
@@ -26,7 +27,7 @@ function init_hemul(){
 
 
 $( document ).ready(function() {
-	send_props_to_server();
+	send_props_to_server(handle_ajax_succes);
     init_hemul();
 });
 
