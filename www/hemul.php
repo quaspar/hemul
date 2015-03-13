9a1b93014e9eb7cc347f0e3191fce6cfe957aa7a
Rémi Puig <remi.puig@gmail.com>
2015-03-13 21:23:21 +0100

    retrieve län from municipality code

--- www/hemul.php
+++ www/hemul.php
@@ -215,4 +215,33 @@ class hemul {
 		}
 		return $data;
 	}
+
+	private function getLan() {
+		$kommun = $this::scbGetKommun();
+		if (!$kommun) return NULL;
+		$laner = array(
+			'01' => 'Stockholms län',
+			'03' => 'Uppsala län',
+			'04' => 'Södermanlands län',
+			'05' => 'Östergötlands län',
+			'06' => 'Jönköpings län',
+			'07' => 'Kronobergs län',
+			'08' => 'Kalmar län',
+			'09' => 'Gotlands län',
+			'10' => 'Blekinge län',
+			'12' => 'Skåne län',
+			'13' => 'Hallands län',
+			'14' => 'Västra Götalands län',
+			'17' => 'Värmlands län',
+			'18' => 'Örebro län',
+			'19' => 'Västmanlands län',
+			'20' => 'Dalarnas län',
+			'21' => 'Gävleborgs län',
+			'22' => 'Västernorrlands län',
+			'23' => 'Jämtlands län',
+			'24' => 'Västerbottens län',
+			'25' => 'Norrbottens län',
+		);
+		return $laner[substr($kommun, 0, 2)];	
+	}
 }
