431aa8167affd8f5e188e209bd9e297a91caa8bc
quaspar <caspar.behrendt@gmail.com>
2015-03-14 11:52:28 +0100

    skapade directive <hemul-widget>

--- extension/hemul.js
+++ extension/hemul.js
@@ -1,7 +1,33 @@
 (function() {
 // todo: http://learn.ionicframework.com/formulas/backend-data/
   var app = angular.module('Hemul', []);
+  
+  app.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    chrome.extension.getURL('/') + '**',
+    'http://hemul.fria.nu/**'
+  ]);
+});
 
+  app.directive('hemulWidget', function() {
+    return {
+        restrict: 'E',
+        templateUrl: chrome.extension.getURL('/widget.tpl'),
+        replace: true /* ,
+		link: function (scope, element, attrs) {
+      		var attr;
+      		for (attr in attrs.$attr) {
+        		if(attrs.hasOwnProperty(attr)){
+          			element.attr(attr, attrs[attr]);
+        		}
+      		}
+    	} */
+  	};
+  });
+  
   app.controller('TabController', function(){
     this.tab = 0;
 
@@ -25,8 +51,8 @@
   	$http.get('http://hemul.fria.nu/ajaxhandler.php?directive=rainfall&properties=' + widgetCtrl.properties)
   		.success(function(data){
   			if (data.status == 1){
-  				console.log(data);
-  				widgetCtrl.widgets.rainfall = data.rainfall;
+  				widgetCtrl.widgets.rainfall.data = data.rainfall;
+  				widgetCtrl.widgets.rainfall.name = "rainfall";
   				widgetCtrl.widgets.rainfall.status = data.status;
   				console.log(widgetCtrl.widgets.rainfall); // debug  	
   			}			
@@ -37,7 +63,8 @@
   	$http.get('http://hemul.fria.nu/ajaxhandler.php?directive=electionresults&properties=' + widgetCtrl.properties)
   		.success(function(data){
   			if (data.status == 1){
-  				widgetCtrl.widgets.electionresults = data.electionresults;
+  				widgetCtrl.widgets.electionresults.data = data.electionresults;
+  				widgetCtrl.widgets.electionresults.name = "electionresults";
   				widgetCtrl.widgets.electionresults.status = data.status;
   				console.log(widgetCtrl.widgets.electionresults); // debug  	
   			}
---
+++ extension/widget.tpl
@@ -0,0 +1,1 @@
+<p>cbtest: {{widget.name}}</p>
