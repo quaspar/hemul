a1ee3eddec0e8e520602c0a0087e77eb4846f315
Rémi Puig <remi.puig@gmail.com>
2015-03-14 15:59:15 +0100

    modals footer

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -42,7 +42,7 @@ function init_hemul(){
 function init_hemul2() {
 	var hemulHtml = chrome.extension.getURL('/hemul.html');
 	var hemulDiv = '<div id="hemul" ng-app="Hemul" ng-include="\'' + hemulHtml + '\'"></div>';
-  	var hemulModal = '<div id="hemulModal" class="modal fade" role="dialog" aria-labelledby="mymodallabel" aria-hidden=true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title" id="myModalLabel">Hemul</h4></div><div class="modal-body">' + hemulDiv + '</div><div class="modal-footer">Credits</div></div></div></div></div>';
+  	var hemulModal = '<div id="hemulModal" class="modal fade" role="dialog" aria-labelledby="mymodallabel" aria-hidden=true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h2 class="modal-title" id="myModalLabel">Hemul</h2></div><div class="modal-body">' + hemulDiv + '</div><div class="modal-footer">' + hemulFooter() + '</div></div></div></div></div>';
 	$('#page-container').append(hemulModal);
 	$("#item-actions").append("<a class='button' href='#' id='hemulPopup' style='margin-left: 8px;'><span><i class='fa fa-line-chart'> </i>Hemul");
 	$('#item-actions').on('click', '#hemulPopup', function() {
@@ -52,8 +52,11 @@ function init_hemul2() {
 	
 }
 
-function hemulModal() {
-
+function hemulFooter() {
+	return	'<img src="' + chrome.extension.getURL('/images/af.jpg') + '" style="margin-right: 8px;max-height: 25px;">' +
+		'<img src="' + chrome.extension.getURL('/images/rka.png') + '" style="margin-right: 8px;max-height: 25px;">' +
+		'<img src="' + chrome.extension.getURL('/images/smhi.png') + '" style="margin-right: 8px;max-height: 25px;">' +
+		'<img src="' + chrome.extension.getURL('/images/scb.png') + '" style="margin-right: 8px; max-height: 25px;">';
 }
 
 //$( document ).ready(function() {
