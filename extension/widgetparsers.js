cc5da9b32e7a5ac0fa44b5914c7e220f29810006
Rémi Puig <remi.puig@gmail.com>
2015-03-14 18:34:08 +0100

    first try first widget..

--- extension/widgetparsers.js
+++ extension/widgetparsers.js
@@ -3,5 +3,60 @@ function hemul_rainfall(widget){
 }
 
 function hemul_electionresults(widget){
-	return widget.id;
+	var obj={"markup": '<div id="electionsContainer"></div>', "callback": "electionresultsCallback"};
+	return obj;
+}
+
+function electionresultsCallback(data) {
+	var data = data.data;
+	var dataArray = objToArray(data.data);
+
+	function objToArray(obj) {
+	    var result = [];
+	    for (var key in obj) {
+	       if (obj.hasOwnProperty(key) && $.isNumeric(obj[key])) {
+	           result.push([key,parseFloat(obj[key])]);
+	       }
+	    }
+    	    return result;
+	}
+
+
+	Highcharts.setOptions({
+		colors: ['#b02522', '#c13b38', '#acc768', '#e7d960', '#78ae5a', '#378cab', '#366da3', '#88c7d9', '#c0c0c0']
+	});
+
+	$('#container').highcharts({
+	        chart: {
+	            plotBackgroundColor: null,
+	            plotBorderWidth: null,
+	            plotShadow: false
+	        },
+	        title: {
+	            text: 'valresultatet riksdagsval, 2014'
+	        },
+	        tooltip: {
+	            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+	        },
+	        plotOptions: {
+	            pie: {
+	                allowPointSelect: true,
+	                cursor: 'pointer',
+	                dataLabels: {
+	                    enabled: true,
+	                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+	                    style: {
+	                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+	                    }
+	                }
+	            }
+	        },
+	        series: [{
+	            type: 'pie',
+				title: 'val2014',
+	            data: dataArray,
+	
+	        }]
+	
+	    });
 }
